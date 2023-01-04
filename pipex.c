0de33828494374925cc027dad9d6f1b9cc29cb24
ahlyel-amine <ahlyel-amine@users.noreply.github.com>
2023-01-04 12:29:36 +0100

    destroy function

--- pipex.c
+++ pipex.c
@@ -1,5 +1,4 @@
-// #include "pipex.h"
-#include "libft/libft.h"
+#include "pipex.h"
 
 #include <errno.h>
 #include <fcntl.h>
@@ -27,46 +26,72 @@ char *check_command(char **path, char *command)
 	return (cmd_path);
 }
 
+int	child_process1(char **argv, char **envp, t_pipex pipex)
+{
+	pipex.pid1 = fork();
+	if (!pipex.pid1)
+	{
+		dup2(pipex.fd[1], STDOUT_FILENO);
+		close(pipex.fd[1]);
+		close(pipex.fd[0]);
+		dup2(pipex.infile, STDIN_FILENO);
+		execve(pipex.cmd1, ft_split(argv[2], ' '), envp);
+		return (1);
+	}
+	return (0);
+}
+
+int	child_process2(char **argv, char **envp, t_pipex pipex)
+{
+	pipex.pid2 = fork();
+	if (!pipex.pid2)
+	{
+		dup2(pipex.fd[0], STDIN_FILENO);
+		close(pipex.fd[1]);
+		close(pipex.fd[0]);
+		dup2(pipex.outfile, STDOUT_FILENO);
+		execve(pipex.cmd2, ft_split(argv[3], ' '), envp);
+		return (1);
+	}
+	return (0);
+}
+
+int	destroy(int open, int pipe, int command, int child_process)
+{
+	if (open)
+		return (perror("Error file"), 1);
+	else if (pipe)
+		return (perror("Error pipe"), 1);
+	else if (command)
+		return (perror("Error Command not found"), 1);
+	else if (child_process)
+		return (perror("Error child process"), 1);
+}
+
 int main(int argc, char **argv, char **envp)
 {
-	char	**path;
-	char	*cmd1;
-	char	*cmd2;
-	int		infile;
-	int		outfile;
-	int		ppid;
-	int		cpid;
-	int		fd[2];
+	t_pipex	pipex;
 
 	unlink(argv[argc - 1]);
-	infile = open(argv[1], O_CREAT | O_RDONLY, 0644);
-	outfile = open(argv[argc - 1], O_CREAT | O_RDWR, 0644);
-	if (pipe(fd) == -1)
-		return (perror("Error"), 1);
-	path = ft_split(&envp[6][5], ':');
-	cmd1 = check_command(path, *ft_split(argv[2], ' '));
-	cmd2 = check_command(path, *ft_split(argv[3], ' '));
-	ppid = fork();
-	if (!ppid)
-	{
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[1]);
-		close(fd[0]);
-		dup2(infile, STDIN_FILENO);
-		execve(cmd1, ft_split(argv[2], ' '), envp);
-	}
-	cpid = fork();
-	if (!cpid)
-	{
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[1]);
-		close(fd[0]);
-		dup2(outfile, STDOUT_FILENO);
-		execve(cmd2, ft_split(argv[3], ' '), envp);
-	}
-	close(fd[0]);
-	close(fd[1]);
-	waitpid(ppid, NULL, 0);
-	waitpid(cpid, NULL, 0);
+	pipex.infile = open(argv[1], O_CREAT | O_RDONLY, 0644);
+	pipex.outfile = open(argv[argc - 1], O_CREAT | O_RDWR, 0644);
+	if (pipex.infile < 0 || pipex.outfile < 0)
+		return (destroy(1, 0, 0, 0));
+	if (pipe(pipex.fd) == -1)
+		return (close(pipex.fd[0]), close(pipex.fd[1]),
+		destroy(0, 1, 0, 0));
+	pipex.path = ft_split(&envp[6][5], ':');
+	pipex.cmd1 = check_command(pipex.path, *ft_split(argv[2], ' '));
+	pipex.cmd2 = check_command(pipex.path, *ft_split(argv[3], ' '));
+	if (!pipex.cmd1 || !pipex.cmd2)
+		return (close(pipex.fd[0]), close(pipex.fd[1]),
+		destroy(0, 0, 1, 0));
+	if (!child_process1(argv, envp, pipex) || !child_process2(argv, envp, pipex))
+		return (close(pipex.fd[0]), close(pipex.fd[1]),
+		destroy(0, 0, 0, 1));
+	close(pipex.fd[0]);
+	close(pipex.fd[1]);
+	waitpid(pipex.pid1, NULL, 0);
+	waitpid(pipex.pid2, NULL, 0);
 	return 0;
 }
--- pipex.h
+++ pipex.h
@@ -3,4 +3,16 @@
 
 #include "libft/libft.h"
 
+typedef struct s_pipex
+{
+	char	**path;
+	char	*cmd1;
+	char	*cmd2;
+	int		infile;
+	int		outfile;
+	int		pid1;
+	int		pid2;
+	int		fd[2];
+}	t_pipex;
+
 #endif
