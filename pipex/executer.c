b7640472c5293c7bec434c11267c8ee06e3328a4
ahlyel-amine <ahlyel-amine@users.noreply.github.com>
2023-01-15 01:01:39 +0100

    pas

--- pipex/executer.c
+++ pipex/executer.c
@@ -6,37 +6,38 @@
 /*   By: aahlyel <aahlyel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 21:10:36 by aahlyel           #+#    #+#             */
-/*   Updated: 2023/01/13 19:54:53 by aahlyel          ###   ########.fr       */
+/*   Updated: 2023/01/14 17:55:03 by aahlyel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/pipex.h"
 #include "headers/libft.h"
 
-int	child_process(t_args *arguments, t_list **garbg, int pid_indice, char **envp)
-{
-	(*arguments).pids[pid_indice] = fork();
-	if (!(*arguments).pids[pid_indice])
-	{
-		dup2((*arguments).fd[1], STDOUT_FILENO);
-		close((*arguments).fd[1]);
-		close((*arguments).fd[0]);
-		dup2((*arguments).infile, STDIN_FILENO);
-		execve((*arguments).cmds_path[pid_indice], (*arguments).cmds[pid_indice], envp);
-		return (1);
-	}
-	return (0);
-}
+// int	child_process(t_args *arguments, t_list **garbg, int pid_indice, char **envp)
+// {
+// 	(*arguments).pids[pid_indice] = fork();
+// 	if (!(*arguments).pids[pid_indice])
+// 	{
+// 		dup2((*arguments).infile, STDIN_FILENO);
+// 		dup2((*arguments).fd[1], STDOUT_FILENO);
+// 		close((*arguments).fd[1]);
+// 		close((*arguments).fd[0]);
+// 		execve((*arguments).cmds_path[pid_indice], (*arguments).cmds[pid_indice], envp);
+// 		return (1);
+// 	}
+// 	return (0);
+// }
 
 int	first_child_process(t_args *arguments, t_list **garbg, int pid_indice, char **envp)
 {
+	puts("a");
 	(*arguments).pids[pid_indice] = fork();
 	if (!(*arguments).pids[pid_indice])
 	{
-		dup2((*arguments).infile, STDIN_FILENO);
 		dup2((*arguments).fd[1], STDOUT_FILENO);
 		close((*arguments).fd[1]);
 		close((*arguments).fd[0]);
+		dup2((*arguments).infile, STDIN_FILENO);
 		execve((*arguments).cmds_path[pid_indice], (*arguments).cmds[pid_indice], envp);
 		return (1);
 	}
@@ -45,13 +46,14 @@ int	first_child_process(t_args *arguments, t_list **garbg, int pid_indice, char 
 
 int	middle_childs_process(t_args *arguments, t_list **garbg, int pid_indice, char **envp)
 {
+	puts("b");
 	(*arguments).pids[pid_indice] = fork();
 	if (!(*arguments).pids[pid_indice])
 	{
-		dup2((*arguments).fd[1], STDIN_FILENO);
-		dup2((*arguments).fd[0], STDOUT_FILENO);
+		dup2((*arguments).fd[0], STDIN_FILENO);
 		close((*arguments).fd[1]);
 		close((*arguments).fd[0]);
+		dup2((*arguments).fd[1], STDOUT_FILENO);
 		execve((*arguments).cmds_path[pid_indice], (*arguments).cmds[pid_indice], envp);
 		return (1);
 	}
@@ -60,6 +62,12 @@ int	middle_childs_process(t_args *arguments, t_list **garbg, int pid_indice, cha
 
 int	last_child_process(t_args *arguments, t_list **garbg, int pid_indice, char **envp)
 {
+	puts("c");
+	// waitpid((*arguments).pids[pid_indice - 1], NULL, 0);
+	// 	char *v;
+	// v = malloc(4000);
+	// read((*arguments).fd[1], v, 4000);
+	// write(1, v, 4000);
 	(*arguments).pids[pid_indice] = fork();
 	if (!(*arguments).pids[pid_indice])
 	{
--- pipex/main.c
+++ pipex/main.c
@@ -6,7 +6,7 @@
 /*   By: aahlyel <aahlyel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 14:01:03 by aahlyel           #+#    #+#             */
-/*   Updated: 2023/01/13 19:40:51 by aahlyel          ###   ########.fr       */
+/*   Updated: 2023/01/14 17:55:34 by aahlyel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,56 +18,33 @@ int main(int argc, char **argv, char **envp)
 	t_args	*arguments;
 	t_list	*garbg;
 	int	i;
-
+	int	fd[2][2];
+	// fd = malloc(2 * sizeof(int *));
 	i = 1;
 	garbg = NULL;
 	arguments = ft_malloc(malloc(sizeof(t_args)), &garbg);
 	(*arguments).path = ft_split(env_path(envp, &garbg), ':', &garbg); //need impelement garbage collector in ft_split
 	get_args(&arguments, argv, argc, &garbg);
-	// if (pipe(arguments->fd) == -1)
-	// 	ft_exit("Error function pipe failure", &garbg);
+	if (pipe(arguments->fd) == -1)
+		ft_exit("Error function pipe failure", &garbg);
 	(*arguments).pids = ft_malloc(malloc(sizeof(int) * argc - 2), &garbg);
-	// // (*arguments).pids[argc - 3] = ;
-	if (pipe((*arguments).fd) == -1)
-		ft_exit("Error function pipe() failure", &garbg);
 	first_child_process(arguments, &garbg, 0, envp);
+	printf("%d\n", waitpid((*arguments).pids[0], NULL, 0));
 	while (i < argc - 4)
 	{
-		middle_childs_process(arguments, &garbg, i++, envp);
-		// printf("%d\n", waitpid((*arguments).pids[i++], NULL, 0));
+		middle_childs_process(arguments, &garbg, i, envp);
+		printf("%d\n", waitpid((*arguments).pids[i++], NULL, 0));
 	}
 	last_child_process(arguments, &garbg, i, envp);
-	// int j = 0;
-	// while (j< argc - 4)
-	// {
-	// printf("a\n%d\n", waitpid((*arguments).pids[j++], NULL, 0));
-	// }
-
-	// waitpid((*arguments).pids[i], NULL, 0);
-	// waitpid((*arguments).pids[i], NULL, 0);
+	printf("%d\n", waitpid((*arguments).pids[i], NULL, 0));
+	// close((*arguments).fd[0]);
+	// close((*arguments).fd[1]);
+			close(fd[1][0]);
+		close(fd[0][0]);
+		close(fd[1][0]);
+		close(fd[1][1]);
+	// while (j< argc - 3)
+	// 	printf("%d\n", (*arguments).pids[j++]);
 	ft_lstclear(&garbg, free);
-	// int  j = 0, k = 0;
-	// while (*((*arguments).cmds_path + k))
-	// {
-	// 	printf("%s\n", *((*arguments).cmds_path + k++));
-	// }
-	// while (((*arguments).cmds)[i])
-	// {
-	// 	j = 0;
-	// 	while (((*arguments).cmds)[i][j])
-	// 	{
-	// 		printf("%s\n", ((*arguments).cmds)[i][j++]);
-	// 	}
-	// 	i++;
-	// }
-	// char **ls;
-	// ls = malloc(3 * sizeof(char *));
-	// *ls = malloc(3);
-	// *(ls + 1) = malloc(5);
-	// ft_memcpy(*ls, "ls\0", 3);
-	// ft_memcpy(*(ls + 1), "-lha\0", 5);
-	// *(ls + 2) = NULL;
-	// int i = -999;
-	// i = execve("/bin/ls", ls, envp);
 	return 0;
 }
